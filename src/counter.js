a0b2c19bb31203ee7fa61629e8673899cb81dad6
MalcolmGomes <malcolm.gomes@ryerson.ca>
2020-05-16 16:03:50 -0400

    Finished React tutorial and Redux starter in counter.js.

--- src/counter.js
+++ src/counter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 
 const initialState = {
   count: 0
